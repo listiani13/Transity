f3b1abab0707a2f1ce4fb73a60e3f8ec627df2f6
listiani13 <listiliz13@gmail.com>
2018-06-02 19:56:15 +0700

    delete unused stuff and delete package menu context

--- src/scenes/HomeScene/HomeScene.js
+++ src/scenes/HomeScene/HomeScene.js
@@ -9,14 +9,6 @@ import {
   TouchableOpacity,
   InteractionManager,
 } from 'react-native';
-import {SimpleLineIcons} from '@expo/vector-icons';
-import {
-  MenuContext,
-  Menu,
-  MenuOptions,
-  MenuOption,
-  MenuTrigger,
-} from 'react-native-popup-menu';
 // $FlowFixMe
 import {NavigationActions} from 'react-navigation';
 import {Text} from '../../components/CoreComponents';
@@ -24,7 +16,6 @@ import FloatingButton from './components/FloatingButton';
 import Trip from './components/Trip';
 
 import {baseColors, BACKGROUND_GREY} from '../../constants/colors';
-import {baseTextStyle} from '../../constants/text';
 
 import type {TripDatum} from '../../types';
 
@@ -53,32 +44,9 @@ export default class HomeScene extends Component<Props, State> {
       headerTintColor: 'white',
       headerRight: (
         <View>
-          <MenuContext
-            style={{
-              flex: 1,
-              alignItems: 'center',
-              justifyContent: 'center',
-              marginRight: PADDING_HORIZONTAL,
-            }}
-          >
-            <TouchableOpacity onPress={params.logout}>
-              <Text style={{color: 'white'}}>Sign Out</Text>
-              {/* <Menu style={{zIndex: 99}}>
-                <MenuTrigger style={{zIndex: 99}}>
-                  <View style={{width: 80, alignItems: 'flex-end'}}>
-                    <SimpleLineIcons
-                      name="options-vertical"
-                      size={baseTextStyle.LARGE_FONT_SIZE}
-                      color={baseColors.white}
-                    />
-                  </View>
-                </MenuTrigger>
-                <MenuOptions style={{zIndex: 99}}>
-                  <MenuOption onSelect={params.logout} text="Log Out" />
-                </MenuOptions>
-              </Menu> */}
-            </TouchableOpacity>
-          </MenuContext>
+          <TouchableOpacity onPress={params.logout}>
+            <Text style={{color: 'white'}}>Sign Out</Text>
+          </TouchableOpacity>
         </View>
       ),
     };
@@ -114,26 +82,6 @@ export default class HomeScene extends Component<Props, State> {
     data: [],
   };
 
-  // _getInitialState = () => {
-  //   // this.props.navigation.setParams({logout: this._logout});
-  //   AsyncStorage.getItem('myTrip').then((data) => {
-  //     if (data != null) {
-  //       data = JSON.parse(data);
-  //     } else {
-  //       data = [];
-  //     }
-  //     let newTrip: Array<TripDatum> = data.map((data, index) => {
-  //       return {
-  //         id: index.toString(),
-  //         title: data.name,
-  //         isOpen: false,
-  //         sight: data.route.length,
-  //         route: data.route,
-  //       };
-  //     });
-  //     this.setState({data: newTrip});
-  //   });
-  // };
   _getInitialState = async () => {
     // this.props.navigation.setParams({logout: this._logout});
     let myTrip = await AsyncStorage.getItem('myTrip');
@@ -169,18 +117,21 @@ export default class HomeScene extends Component<Props, State> {
     return (
       <View style={styles.container}>
         <ScrollView width="100%" contentContainerStyle={{padding: 10}}>
-          {data.map((item, id) => {
-            return (
-              <Trip
-                key={id}
-                title={item.title}
-                sights={item.sight}
-                isOpen={item.isOpen}
-                onToggleRoute={() => this._onToggleRoute(item.id)}
-                routeData={item.route}
-              />
-            );
-          })}
+          {data
+            .slice(0)
+            .reverse()
+            .map((item, id) => {
+              return (
+                <Trip
+                  key={id}
+                  title={item.title}
+                  sights={item.sight}
+                  isOpen={item.isOpen}
+                  onToggleRoute={() => this._onToggleRoute(item.id)}
+                  routeData={item.route}
+                />
+              );
+            })}
         </ScrollView>
         <FloatingButton
           onPress={() => this.props.navigation.navigate('AddNewTrip')}
